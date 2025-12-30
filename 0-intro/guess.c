6f7dabdba62b399d2233574c7312a94b0a217322
expiid <expiid@outlook.com>
2025-12-30 12:31:18 +0800

    0-intro
    # Conflicts:
    #	.gitignore

---
+++ 0-intro/guess.c
@@ -0,0 +1,62 @@
+//
+// Created by ExpiiD on 2025/12/30.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int high = 100;
+    int chance = 7;
+    /*
+     * print the rule of the game
+     */
+    printf("The computer will generate a random number between 1 and %d.\n"
+           "You have %d chances.\n",
+        high, chance);
+
+     /*
+     * generate a random number
+     */
+    srand(time(NULL));
+    // 0 …… RAND_MAX
+    // 1 ... high
+    int secret = rand() % high + 1;
+    printf("secret is %d\n", secret);
+
+    while (chance > 0)
+    {
+        /*
+         * let the player enter his/her guess number
+         */
+        printf("Enter your guess.\n");
+        /*
+         * store the guess number
+         * compare it with the secret
+         * and inform the player of the result
+         */
+        int guess;
+        scanf("%d", &guess);
+
+        if (guess == secret)
+        {
+            printf("You win!\n");
+            break;
+        } else if (guess > secret)
+        {
+            printf("guess > secret\n");
+        } else
+        {
+            printf("guess < secret\n");
+        }
+
+        /*
+         * loop: repeat until the player wins or lose
+         */
+        chance--;
+    }
+
+    return 0;
+}
---
+++ 0-intro/hello.c
@@ -0,0 +1,8 @@
+//
+// Created by ExpiiD on 2025/12/29.
+//
+#include <stdio.h>
+int main()
+{
+    printf("Hello World!\n");
+}
